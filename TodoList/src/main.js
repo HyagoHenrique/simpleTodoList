fa0e4b80c42a4e07d87cedc754ce276ff9e8dc4c
Hyago <hyagoh@icloud.com>
2021-03-18 15:50:35 -0300

    Sconde Stage :)!
    almost finish, updates come some day.

--- TodoList/src/main.js
+++ TodoList/src/main.js
@@ -1,2 +1,48 @@
 import Todos from "./api/todos";
 import "./assets/css/main.css";
+import { createApp } from "vue";
+
+const apiTodos = new Todos();
+
+const app = createApp({
+  data() {
+    return {
+      todos: [],
+      form: {
+        text: "",
+        done: false,
+      },
+      loading: false,
+    };
+  },
+  async created() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      try {
+        this.loading = true;
+        this.todos = await apiTodos.index();
+      } catch (error) {}
+    },
+    async createTodo() {
+      const data = await apiTodos.store(this.form);
+      this.todos.push(data);
+      this.form.text = "";
+      this.form.done = false;
+    },
+    async toggleTodoStatus(todo) {
+      const data = await apiTodos.update({ ...todo, done: !todo.done });
+
+      const index = this.todos.findIndex(({ id }) => id === data.id);
+      this.todos[index] = data;
+    },
+    async destroyTodo(id) {
+      await apiTodos.destroy({ id });
+      const index = this.todos.findIndex((todo) => todo.id === id);
+      this.todos.splice(index, 1);
+    },
+  },
+});
+
+app.mount("#app");
--- TodoList/webpack.config.js
+++ TodoList/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: ["@babel/polyfill", path.resolve(__dirname, "./src/main.js")],
@@ -19,4 +20,15 @@ module.exports = {
       },
     ],
   },
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.esm-bundler.js",
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      _VUE_OPTIONS_API_: true,
+      _VUE_PROD_DEVTOOLS_: false,
+    }),
+  ],
 };
